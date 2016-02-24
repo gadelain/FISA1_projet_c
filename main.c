ecf13d4999ba157f6997a5ac165673991317b087
gadelain <gui.adelain@gmail.com>
2016-02-24 23:56:52 +0100

    Added files via upload

--- main.c
+++ main.c
@@ -2,14 +2,18 @@
 #include <stdlib.h>
 
 #include "annexe.h"
+#include "cryptage.h"
+#include "decryptage.h"
 
 int main(void)
 {
 	TABLEAU recup = { 0, NULL };
 
-	recup = recup_fichier("test.txt");
+	//recup = recup_fichier("test.txt");
 
 	//free_tableau(&recup);
+	cryptage_cesar("fichier.txt");
+	decryptage_cesar("fichier_crypt.txt");
 
 	system("pause");
 	return 0;
