e4d917fb0ac112722c52018f9ba38346a7871cc1
gadelain <gui.adelain@gmail.com>
2016-03-09 23:48:39 +0100

    Delete decryptage.h

--- decryptage.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#define INC_DECRYPTAGE_
-
-int decryptage_cesar(const char *str);
-int decryptage_image_pgm(const char *str, const char *fichier_crypt, const char *key_crypt);
-int decryptage_image_ppm(const char *str, const char *fichier_crypt, const char *key_crypt);
