b7a8fe24882d131dc1a9ef476045b050ad3d343b
gadelain <gui.adelain@gmail.com>
2016-03-09 23:50:30 +0100

    Added files via upload

--- main.c
+++ main.c
@@ -1,23 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "annexe.h"
 #include "menu.h"
-#include "cryptage.h"
-#include "decryptage.h"
+
 
 int main(void)
 {
-	TABLEAU recup = { 0, NULL };
+	char choix = '0';
+	char retour_menu = '0';
+	char watchdog = '0';
 
-	//recup = recup_fichier("test.txt");
+	while (watchdog != '1')
+	{
+		while (choix != '1' || choix != '2' || choix != '3' || choix != '4')
+		{
+			printf("===== Projet C FISA 1 : Cryptage / Decryptage =====\n");
+			printf("1 - Cryptage\n");
+			printf("2 - Decryptage\n");
+			printf("3 - Annexe / aide\n");
+			printf("4 - Quitter le programme\n\n");
+			scanf("%c", &choix);
 
-	//free_tableau(&recup);
-	menu_image_crypt();
-	//decryptage_cesar("fichier_crypt.txt");
+			if (choix != '1' || choix != '2' || choix != '3' || choix != '4')
+			{
+				system("cls");
+				printf("Erreur : vous n'avez pas entre un choix correct\n");
+			}
+		}
 
-	//decryptage_image_pgm("fichier_crypt_image.txt", "key.txt");
+		switch (choix)
+		{
+		case '1':
+			break;
+		case '2':
+			break;
+		case '3':
+			retour_menu = menu_annexe();
+			if (retour_menu != 0)
+			{
+				printf("Erreur dans l'execution du programme");
+			}
+			break;
+		case '4':
+			watchdog = '1';
+			break;
+		default:
+			break;
+		}
 
-	system("pause");
+	}
+
 	return 0;
 }
